83b5c71fb033baeeab15f04e5745cffc4f3c0719
Eric <54091924+EricHsia7@users.noreply.github.com>
2025-02-23 15:00:51 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import './interface/theme.css';
 import './interface/index.css';
 
 import './interface/home/field.css';
+import './interface/home/head.css';
 
 window.erichsia7 = {
   initialize: function () {}
